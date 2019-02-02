a0c9a936234e8438d30e0026c809038bc63c8ee5
sily <16009413@qq.com>
2019-02-02 16:44:55 +0800

    员工社区系统   https://github.com/ChinaLHR/JavaQuarkBBS

--- quark-rest/src/main/java/com/quark/rest/utils/Constants.java
+++ quark-rest/src/main/java/com/quark/rest/utils/Constants.java
@@ -7,6 +7,6 @@ package com.quark.rest.utils;
 public class Constants {
     //Upload常量
     public static final String UPLOAD_PATH = "D:\\home\\images\\upload\\";
-    public static final String STATIC_URL ="http://127.0.0.1/images/upload/";
+    public static final String STATIC_URL ="http://120.133.22.231:8097/images/upload/";
 
 }
